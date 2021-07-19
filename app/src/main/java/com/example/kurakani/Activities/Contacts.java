8522f6eb5a80510aca68f85161ca39bc0966fa63
PrashannaR <prashanna.rajbhandari1@gmail.com>
2021-07-19 21:48:22 +0545

    removed contacts

--- app/src/main/java/com/example/kurakani/Activities/Contacts.java
+++ app/src/main/java/com/example/kurakani/Activities/Contacts.java
@@ -32,9 +32,6 @@ public class Contacts extends AppCompatActivity {
 
     ActivityContactsBinding binding;
 
-    RecyclerView recyclerView;
-    ArrayList<ContactModel> arrayList = new ArrayList<ContactModel>();
-    MainAdapter adapter;
 
 
 
@@ -47,9 +44,6 @@ public class Contacts extends AppCompatActivity {
 
         getSupportActionBar().hide();
 
-        recyclerView = binding.recyclerView;
-
-        checkPermission();
 
 
         //bottom nav functioning
@@ -86,76 +80,9 @@ public class Contacts extends AppCompatActivity {
         });
     }
 
-    private void checkPermission() {
-        if(ContextCompat.checkSelfPermission(Contacts.this, Manifest.permission.READ_CONTACTS)
-        != PackageManager.PERMISSION_GRANTED){
 
-            ActivityCompat.requestPermissions(Contacts.this,
-                    new String[]{Manifest.permission.READ_CONTACTS}, 100);
-        }else {
-            getContactsList();
-        }
-    }
 
-    private void getContactsList() {
 
-        Uri uri = ContactsContract.Contacts.CONTENT_URI;
 
-        String sort = ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME +"ASC";
 
-        Cursor cursor = getContentResolver().query(
-                uri, null, null, null, sort
-        );
-
-        if(cursor.getCount() > 0){
-            while (cursor.moveToNext()){
-                String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
-
-                String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-
-                Uri uriPhone = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
-
-                String selection = ContactsContract.CommonDataKinds.Phone.CONTACT_ID
-                        + "=?";
-
-                 Cursor phoneCursor = getContentResolver().query(
-                         uriPhone, null, selection,
-                         new String[]{id}, null
-                 );
-                 if(phoneCursor.moveToNext()){
-                     String number = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-
-                     ContactModel model = new ContactModel();
-
-                     model.setName(name);
-                     model.setNumber(number);
-
-                     arrayList.add(model);
-
-                     phoneCursor.close();
-                 }
-            }
-            cursor.close();
-        }
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        adapter = new MainAdapter(this,arrayList);
-
-        recyclerView.setAdapter(adapter);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull @NotNull String[] permissions, @NonNull @NotNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        if(requestCode == 100 && grantResults.length > 0 && grantResults[0] ==
-        PackageManager.PERMISSION_GRANTED){
-            getContactsList();
-        }else {
-            Toast.makeText(Contacts.this, "Permission Denied", Toast.LENGTH_SHORT).show();
-
-            checkPermission();
-        }
-
-    }
 }
